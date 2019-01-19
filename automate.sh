101b0ef8712e3736c5dcdb626d0a32f48332c3de
MayuBhattu <mayubhattu@gmail.com>
2019-01-20 03:26:35 +0530

    Update code

--- automate.sh
+++ automate.sh
@@ -1,6 +1,5 @@
-cd /home/enlightened/Desktop/try/RPi-Logs
 git add .
-git commit -m "Update logs"
+git commit -m "Update code"
 git fetch upstream
 git merge upstream/master -m " Merge remote-tracking branch 'upstream/master' "
 git push origin master
