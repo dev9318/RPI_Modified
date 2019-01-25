d3028dd4a2bb8cc7c3da36f7b771dcf854bcbdfa
MayuBhattu <mayubhattu@gmail.com>
2019-01-26 03:30:01 +0530

    Update code

--- automate.sh
+++ automate.sh
@@ -4,3 +4,7 @@ git commit -m "Update code"
 git fetch upstream
 git merge upstream/master -m " Merge remote-tracking branch 'upstream/master' "
 git push origin master
+sleep 5;
+killall python
+cd /home/pi/Desktop/RPI_Modified;
+python3 gui.py >> /home/pi/rpi/gui_log.txt 2>&1
